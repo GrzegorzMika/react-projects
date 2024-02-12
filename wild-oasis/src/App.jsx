6d52b50a09c744925e39ef55e699d1f863310926
gregmika <grzegorz.mika@relia-sol.pl>
2024-02-12 23:45:58 +0100

    intro to styled components

--- wild-oasis/src/App.jsx
+++ wild-oasis/src/App.jsx
@@ -1,21 +1,7 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-`;
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: var(--color-brand-500);
@@ -26,7 +12,7 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading as="h2">The Wild Oasis</Heading>
         <Button>Check in</Button>
       </StyledApp>
     </>
