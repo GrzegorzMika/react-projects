b7343cb873bd72545376c07c913a0fdcafdcee74
gregmika <grzegorz.mika@relia-sol.pl>
2024-02-22 22:48:25 +0100

    auth logic

---
+++ wild-oasis/src/features/authentication/useUpdateUser.js
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateCurrentUser } from "../../services/apiAuth";
+
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+  const { mutate: updateUser, isLoading: isUpdating } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: ({ user }) => {
+      toast.success("User updated successfully!");
+      queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+      queryClient.setQueryData(["user"], user);
+    },
+    onError: () => toast.error("User could not be updated!"),
+  });
+  return { updateUser, isUpdating };
+}
